d1e052fa7240e7c9c85e78304bfe11fbea89abb1
jay-hg <1175867101@qq.com>
2020-07-15 23:58:57 +0800

    使用sql解析器

---
+++ src/test/java/study/parser/ParserMain.java
@@ -0,0 +1,28 @@
+package study.parser;
+
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+
+public class ParserMain {
+    public static void main(String[] args) {
+        String sql = "select id,name from user where createTime >= '2020-07-15 12:00:00'";
+
+        SQLStatementParser sqlStatementParser = new MySqlStatementParser(sql);
+
+        SQLStatement sqlStatement = sqlStatementParser.parseStatement();
+
+        MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
+        sqlStatement.accept(visitor);
+
+        visitor.getTables().forEach((k,v)->{
+            System.out.println(k+","+v);
+        });
+
+        visitor.getConditions().forEach(condition -> {
+            System.out.println(condition.getColumn()+","+condition.getOperator()+","+condition.getValues().get(0));
+        });
+
+    }
+}
